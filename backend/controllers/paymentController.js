540dba6a196a61758e40ed6a6f4f174188ddaae5
somraj wadkar <somrajwadkar2@gmail.com>
2026-06-12 15:57:17 +0530

    the new changes in the code

--- backend/controllers/paymentController.js
+++ backend/controllers/paymentController.js
@@ -19,3 +19,28 @@ exports.processPayment = catchAsyncErrors(async (req, res, next) => {
 exports.sendStripeApiKey = catchAsyncErrors(async (req, res, next) => {
   res.status(200).json({ stripeApiKey: process.env.STRIPE_API_KEY });
 });
+
+// Create Product -- Admin
+exports.createProduct = catchAsyncErrors(async (req, res, next) => {
+  let images = [];
+
+  if (typeof req.body.images === "string") {
+    images.push(req.body.images);
+  } else {
+    images = req.body.images;
+  }
+
+  const imagesLinks = [];
+
+  
+
+  req.body.images = imagesLinks;
+  req.body.user = req.user.id;
+
+  const product = await Product.create(req.body);
+
+  res.status(201).json({
+    success: true,
+    product,
+  });
+}); 
