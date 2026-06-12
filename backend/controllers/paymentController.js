a7375462f110891239c3d853e8eb5a941d3c6046
somraj wadkar <somrajwadkar2@gmail.com>
2026-06-12 16:36:10 +0530

    the new and the leatest commit

--- backend/controllers/paymentController.js
+++ backend/controllers/paymentController.js
@@ -2,6 +2,12 @@ const catchAsyncErrors = require("../middleware/catchAsyncErrors");
 
 const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 
+const cloudinary = require("cloudinary");
+
+exports.sendStripeApiKey = catchAsyncErrors(async (req, res, next) => {
+  res.status(200).json({ stripeApiKey: process.env.STRIPE_API_KEY });
+});
+
 exports.processPayment = catchAsyncErrors(async (req, res, next) => {
   const myPayment = await stripe.paymentIntents.create({
     amount: req.body.amount,
@@ -51,5 +57,7 @@ exports.createProduct = catchAsyncErrors(async (req, res, next) => {
   res.status(201).json({
     success: true,
     product,
+
+
   });
 }); 
