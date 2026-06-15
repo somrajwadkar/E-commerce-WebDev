6ea2faa2ef12a47ade7c70e8d93832d8fa082bb5
somraj wadkar <somrajwadkar2@gmail.com>
2026-06-15 17:29:25 +0530

    the new changes

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -62,10 +62,3 @@ let initialState = {
 
 const middleware = [thunk];
 
-const store = createStore(
-  reducer,
-  initialState,
-  composeWithDevTools(applyMiddleware(...middleware))
-);
-
-export default store;
