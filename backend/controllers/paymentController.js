d383acf5860392d5cd0081ac7f8ee3d2886d5123
somraj wadkar <somrajwadkar2@gmail.com>
2026-06-15 17:36:15 +0530

    the

--- backend/controllers/paymentController.js
+++ backend/controllers/paymentController.js
@@ -4,23 +4,6 @@ const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 
 const cloudinary = require("cloudinary");
 
-exports.sendStripeApiKey = catchAsyncErrors(async (req, res, next) => {
-  res.status(200).json({ stripeApiKey: process.env.STRIPE_API_KEY });
-});
-
-exports.processPayment = catchAsyncErrors(async (req, res, next) => {
-  const myPayment = await stripe.paymentIntents.create({
-    amount: req.body.amount,
-    currency: "inr",
-    metadata: {
-      company: "Ecommerce",
-    },
-  });
-
-  res
-    .status(200)
-    .json({ success: true, client_secret: myPayment.client_secret });
-});
 
 exports.sendStripeApiKey = catchAsyncErrors(async (req, res, next) => {
   res.status(200).json({ stripeApiKey: process.env.STRIPE_API_KEY });
