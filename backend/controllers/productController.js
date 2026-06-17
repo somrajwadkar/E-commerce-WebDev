fa330dafe3033c5e96512b6e0f56ea4a2a3f10c5
somraj wadkar <somrajwadkar2@gmail.com>
2026-06-17 11:16:18 +0530

    the new changes in new branch somaa

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -38,6 +38,86 @@ exports.createProduct = catchAsyncErrors(async (req, res, next) => {
   });
 });
 
+exports.getAllProducts = catchAsyncErrors(async (req, res, next) => {
+  const resultPerPage = 8;
+  const productsCount = await Product.countDocuments();
+  
+  const apiFeature = new ApiFeatures(Product.find(), req.query)
+    .search()
+    .filter();
+
+
+
+  let products = await apiFeature.query;
+
+  let filteredProductsCount = products.length;
+
+  apiFeature.pagination(resultPerPage);
+
+  products = await apiFeature.query;
+
+  res.status(200).json({
+    success: true,
+    products,
+    productsCount,
+    resultPerPage,
+    filteredProductsCount,
+  });   
+
+});
+
+
+
+exports.getAllProducts = catchAsyncErrors(async (req, res, next) => {
+
+  const resultPerPage = 8;
+  const productsCount = await Product.countDocuments();
+  
+  const apiFeature = new ApiFeatures(Product.find(), req.query)
+    .search()
+    .filter();  
+
+  let products = await apiFeature.query;
+
+  let filteredProductsCount = products.length;
+
+  apiFeature.pagination(resultPerPage);
+
+  products = await apiFeature.query;
+
+  
+
+  
+});
+
+exports.getAllProducts = catchAsyncErrors(async (req, res, next) => {
+
+  const resultPerPage = 8;
+  const productsCount = await Product.countDocuments();
+  
+  const apiFeature = new ApiFeatures(Product.find(), req.query)
+    .search()
+    .filter();  
+
+  let products = await apiFeature.query;
+
+  let filteredProductsCount = products.length;
+
+  apiFeature.pagination(resultPerPage);
+
+  products = await apiFeature.query;
+
+  res.status(200).json({
+    success: true,
+    products,
+    productsCount,
+    resultPerPage,
+    filteredProductsCount,
+  });   
+
+  
+}); 
+
 // Get All Product
 exports.getAllProducts = catchAsyncErrors(async (req, res, next) => {
   const resultPerPage = 8;
@@ -65,10 +145,28 @@ exports.getAllProducts = catchAsyncErrors(async (req, res, next) => {
 });
 
 // Get All Product (Admin)
+exports.getAdminProducts = catchAsyncErrors(async (req, res, next) => {
+  const products = await Product.find();
 
+  res.status(200).json({
+    success: true,
+    products,
+  });
+});
 
 // Get Product Details
+exports.getProductDetails = catchAsyncErrors(async (req, res, next) => {
+  const product = await Product.findById(req.params.id);
 
+  if (!product) {
+    return next(new ErrorHander("Product not found", 404));
+  }
+
+  res.status(200).json({
+    success: true,
+    product,
+  });
+});
 
 // Update Product -- Admin
 
@@ -234,4 +332,27 @@ exports.deleteReview = catchAsyncErrors(async (req, res, next) => {
   res.status(200).json({
     success: true,
   });
+});
+
+exports.getProductReviews = catchAsyncErrors(async (req, res, next) => {
+  const product = await Product.findById(req.query.id);
+
+  if (!product) {
+    return next(new ErrorHander("Product not found", 404));
+  }
+
+  res.status(200).json({
+    success: true,
+    reviews: product.reviews,
+  });
+});
+
+exports.getProductDetails = catchAsyncErrors(async (req, res, next) => {
+  const product = await Product.findById(req.params.id);
+
+  if (!product) {
+    return next(new ErrorHander("Product not found", 404));
+  }
+
+ 
 });
