b02c4fdac831af3e447da32ec7f6c002d59d9519
somraj wadkar <somrajwadkar2@gmail.com>
2026-06-17 11:14:46 +0530

    the new changes in new branch somaa

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -65,28 +65,10 @@ exports.getAllProducts = catchAsyncErrors(async (req, res, next) => {
 });
 
 // Get All Product (Admin)
-exports.getAdminProducts = catchAsyncErrors(async (req, res, next) => {
-  const products = await Product.find();
 
-  res.status(200).json({
-    success: true,
-    products,
-  });
-});
 
 // Get Product Details
-exports.getProductDetails = catchAsyncErrors(async (req, res, next) => {
-  const product = await Product.findById(req.params.id);
 
-  if (!product) {
-    return next(new ErrorHander("Product not found", 404));
-  }
-
-  res.status(200).json({
-    success: true,
-    product,
-  });
-});
 
 // Update Product -- Admin
 
